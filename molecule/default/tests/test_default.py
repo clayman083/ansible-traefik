b6514503477eb63f139a88f4b2f3860c1766fbe7
Kirill Sumorokov <sumorokov.k@gmail.com>
2020-01-24 23:34:29 +0500

    Add dashboard endpoint

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,3 +11,9 @@ def test_exporter_ready(host):
     cmd = host.run("curl -LI 172.17.0.2:8080/metrics -o /dev/null -w '%{http_code}\n' -s")  # noqa
 
     assert cmd.stdout == '200\n'
+
+
+def test_dashboard_available(host):
+    cmd = host.run("curl -LI 172.17.0.2:8081/dashboard/ -o /dev/null -w '%{http_code}\n' -s")  # noqa
+
+    assert cmd.stdout == '200\n'
