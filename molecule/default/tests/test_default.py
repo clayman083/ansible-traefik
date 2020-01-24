1f419a7b7c82f6af8f8a801a761120a70b3c8d27
Kirill Sumorokov <sumorokov.k@gmail.com>
2020-01-24 18:33:39 +0500

    Initial project structure

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,13 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_exporter_ready(host):
+    cmd = host.run("curl -LI 172.17.0.2:8080/metrics -o /dev/null -w '%{http_code}\n' -s")  # noqa
+
+    assert cmd.stdout == '200\n'
